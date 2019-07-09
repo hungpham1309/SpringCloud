330bbb4f96bdcc1572bf69dfedd68c9c51bca350
hungpham <hungpham1309@outlook.com>
2019-07-09 17:28:58 +0700

    Rename project

--- zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/example/zuulgateway/ZuulGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.zullgateway;
+package com.example.zuulgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ZullGatewayApplication {
+public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZullGatewayApplication.class, args);
+        SpringApplication.run(ZuulGatewayApplication.class, args);
     }
 
 }
